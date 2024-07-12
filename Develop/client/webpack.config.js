fffbea2e4cd60bd58217ff30154e3ec57fba7b47
B Water <brennw47@gmail.com>
2024-07-12 12:53:04 -0500

    feat(build): configure Webpack plugins for HTML generation, service worker, and PWA manifest  - Add `HtmlWebpackPlugin` to generate `index.html` and inject bundles. - Use `InjectManifest` to inject custom service worker script `src-sw.js`. - Configure `WebpackPwaManifest` to create a PWA manifest with app details, colors, start URL, and icons. - Add module rules to handle CSS and JavaScript files:   - Use `style-loader` and `css-loader` for `.css` files.   - Use `babel-loader` with `@babel/preset-env`, `@babel/plugin-transform-runtime`, and `@babel/plugin-proposal-object-rest-spread` for `.js` and `.mjs` files, excluding `node_modules`.
    feat(build): configure Webpack plugins for HTML generation, service worker, and PWA manifest
    
    - Add `HtmlWebpackPlugin` to generate `index.html` and inject bundles.
    - Use `InjectManifest` to inject custom service worker script `src-sw.js`.
    - Configure `WebpackPwaManifest` to create a PWA manifest with app details, colors, start URL, and icons.
    - Add module rules to handle CSS and JavaScript files:
      - Use `style-loader` and `css-loader` for `.css` files.
      - Use `babel-loader` with `@babel/preset-env`, `@babel/plugin-transform-runtime`, and `@babel/plugin-proposal-object-rest-spread` for `.js` and `.mjs` files, excluding `node_modules`.

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -1,31 +1,82 @@
 // These are the dependencies for webpack.
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+const { InjectManifest } = require("workbox-webpack-plugin");
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
 // This exports the configuration for webpack.
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: "development",
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+      main: "./src/js/index.js",
+      install: "./src/js/install.js",
     },
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
     },
     plugins: [
-      
+      // This Webpack plugin generates our html file and injects the bundles.
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+        filename: "./index.html",
+        chunks: ["main"],
+      }),
+      // This injects our customer service worker into the service worker file.
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
+      }),
+      // This creates the manifest file for our PWA.
+      new WebpackPwaManifest({
+        // Fingerprints are used to cache bust the service worker.
+        fingerprints: false,
+        // Inject is set to true to inject the manifest into the html file.
+        inject: true,
+        // This is the manifest file name.
+        name: "Just Another Text Editor",
+        // This is the short name of the app.
+        short_name: "JATE",
+        // This is the description of the app.
+        description: "A simple text editor",
+        background_color: "#01579b",
+        theme_color: "#01579b",
+        // This is the start url of the app.
+        start_url: "/",
+        // This is the display mode of the app.
+        publicPath: "./",
+        icons: [
+          {
+            src: path.resolve("src/assets/icons/icon-512x512.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons"),
+          },
+        ],
+      }),
     ],
 
     module: {
-      rules: [
-        
-      ],
+      // The rules in the module are used to define how webpack should handle different file types.
+      rules: [{
+        // Regex to find the css files.
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        // Regex to find the js files.
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+            plugins: ["@babel/plugin-transform-runtime", "@babel/plugin-proposal-object-rest-spread"],
+          },
+        },
+      }],
     },
   };
 };
