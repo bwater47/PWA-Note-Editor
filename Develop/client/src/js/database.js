2ae06205d693eb5cce815dc9d802b5a4f1aee834
B Water <brennw47@gmail.com>
2024-07-12 12:27:23 -0500

    feat(src-sw): add asset caching with CacheFirst strategy  Added the asset caching to the src-sw.js to register a route to cache assets like stylesheets, scripts, and web workers using the CacheFirst strategy, checking if the request's destination is one of "style", "script", or "worker". It configures the CacheFirst strategy with a cache name "asset-cache" and plugins including CacheableResponsePlugin for caching specific status codes and ExpirationPlugin to set a 30-day maximum age for cached assets. Added comments to all of the code. Implemented the offlineFallback strategy; however, I'm not certain it's correct yet and will fix it.
    feat(src-sw): add asset caching with CacheFirst strategy
    
    Added the asset caching to the src-sw.js to register a route to cache assets like stylesheets, scripts, and web workers using the CacheFirst strategy, checking if the request's destination is one of "style", "script", or "worker". It configures the CacheFirst strategy with a cache name "asset-cache" and plugins including CacheableResponsePlugin for caching specific status codes and ExpirationPlugin to set a 30-day maximum age for cached assets. Added comments to all of the code. Implemented the offlineFallback strategy; however, I'm not certain it's correct yet and will fix it.

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -1,29 +1,39 @@
+// Importing the openDB method from the idb package to create a database.
 import { openDB } from "idb";
-
+// This function is used to initialize the database.
 const initdb = async () =>
+  // This function is used to open the database and create an object store.
   openDB("jate", 1, {
+    // This function is used to upgrade the database and create an object store if it does not exist.
     upgrade(db) {
       if (db.objectStoreNames.contains("jate")) {
         console.log("jate database already exists");
         return;
       }
+      // This function is used to create an object store with a key path and auto increment if it does not exist.
       db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
       console.log("jate database created");
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
+// This function is for updating the database with the content.
 export const putDb = async (id, content) => {
   console.log("Updated the database with the following content:", content);
+  // This function is used to open the database and create a transaction.
   const jateDb = await openDB("jate", 1);
+  // This function is used to create a transaction and get the object store.
   const tx = jateDb.transaction("jate", "readwrite");
+  // This function is used to pass the object store to the store variable.
   const store = tx.objectStore("jate");
+  // This function is passing the store variable to the request variable, and the request variable is used to put the content in the database.
   const request = store.put({ id: id, jate: content });
   const result = await request;
   console.log("Data saved to the database:", result);
 };
 
 // TODO: Add logic for a method that gets all the content from the database
+// This function is for getting all of the data from the database.
 export const getDb = async () => {
   console.log("Retrieved all of the data from the database");
   const jateDb = await openDB("jate", 1);
